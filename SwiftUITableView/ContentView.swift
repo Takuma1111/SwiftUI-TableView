07745d820ad49056f8c256dd7c52b4fb10f107ca
Takuma <takuma012011@gmail.com>
2020-09-16 21:28:17 +0900

    Initial Commit

---
+++ SwiftUITableView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITableView
+//
+//  Created by 村上拓麻 on 2020/09/16.
+//  Copyright © 2020 村上拓麻. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
